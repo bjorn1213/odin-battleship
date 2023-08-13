2cb9e82878729931fced25be7f084446f781dbc1
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-08-13 16:46:01 +0200

    misc

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  optimization: {
+    runtimeChunk: "single",
+    minimize: false,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+  plugins: [new HtmlWebpackPlugin()],
   module: {
     rules: [
       {
